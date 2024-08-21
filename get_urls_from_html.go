b6de64489b4ad1705c4f60a87b4edfbedd28ab59
linusbayere <linusbayere@gmail.com>
2024-08-21 09:05:02 -0400

    get urls from html

---
+++ get_urls_from_html.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"net/url"
+	"strings"
+
+	"golang.org/x/net/html"
+)
+
+func getURLsFromHTML(htmlBody, rawBaseURL string) ([]string, error) {
+	baseURL, err := url.Parse(rawBaseURL)
+	if err != nil {
+		return nil, fmt.Errorf("couldn't parse base URL: %v", err)
+	}
+	htmlReader := strings.NewReader(htmlBody)
+	htmlNode, err := html.Parse(htmlReader)
+
+	if err != nil {
+		return []string{}, fmt.Errorf("error parsing htmlReader: %v", err)
+	}
+
+	var traverseNodes func(*html.Node)
+	var urls []string
+	traverseNodes = func(node *html.Node) {
+		if node.Type == html.ElementNode && node.Data == "a" {
+			for _, anchor := range node.Attr {
+				if anchor.Key == "href" {
+					href, err := url.Parse(anchor.Val)
+					if err != nil {
+						fmt.Printf("couldn't parse href '%v': %v\n", anchor.Val, err)
+						continue
+					}
+					resolvedURL := baseURL.ResolveReference(href)
+					urls = append(urls, resolvedURL.String())
+					break
+				}
+			}
+		}
+		for c := node.FirstChild; c != nil; c = c.NextSibling {
+			traverseNodes(c)
+		}
+	}
+	traverseNodes(htmlNode)
+	fmt.Printf("%v", urls)
+
+	return urls, nil
+}
--- main.go
+++ main.go
@@ -4,4 +4,19 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello, World!")
+
+	html := `
+			<html>
+				<body>
+					<a href="/path/one">
+						<span>Boot.dev</span>
+					</a>
+					<a href="https://other.com/path/one">
+						<span>Boot.dev</span>
+					</a>
+				</body>
+			</html>
+			`
+	rawBaseURL := "https://blog.boot.dev"
+	getURLsFromHTML(html, rawBaseURL)
 }
